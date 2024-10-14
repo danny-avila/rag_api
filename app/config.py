317df2e49cda0693313261da9d5bf1ed82310b19
mblye <mblye@kingstreet.com>
2024-10-14 18:21:49 -0400

    Fixing issue w/ Azure embeddings

--- app/config.py
+++ app/config.py
@@ -177,6 +177,7 @@ HF_TOKEN = get_env_variable("HF_TOKEN", "")
 OLLAMA_BASE_URL = get_env_variable("OLLAMA_BASE_URL", "http://ollama:11434")
 AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID", "")
 AWS_SECRET_ACCESS_KEY = get_env_variable("AWS_SECRET_ACCESS_KEY", "")
+RAG_AZURE_CHUNK_SIZE = int(get_env_variable("RAG_AZURE_CHUNK_SIZE", "1"))
 
 ## Embeddings
 
